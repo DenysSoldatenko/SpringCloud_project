a61d78fb0c35624b55549e956333a8aed8a87a5b
DenysSoldatenko <soldatenko911@gmail.com>
2024-05-19 10:00:00 +0300

    Removed the unused annotations in the post-service

--- post-service/src/main/java/org/example/postservice/PostServiceApplication.java
+++ post-service/src/main/java/org/example/postservice/PostServiceApplication.java
@@ -2,16 +2,12 @@ package org.example.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Main application class for the blog module.
  */
 @EnableDiscoveryClient
-@EnableJpaRepositories("org.example.postservice.repositories")
-@EntityScan(value = "org.example.postservice.entities")
 @SpringBootApplication
 public class PostServiceApplication {
 
